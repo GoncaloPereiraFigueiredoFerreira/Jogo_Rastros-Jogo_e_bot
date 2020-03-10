242d7570e96086cf5aa05d12b26de9ef70ba3ae2
GoncaloCoutoDosSantos <a93279@alunos.uminho.pt>
2020-03-10 18:25:19 +0000

    inicilizaçao do estado

--- Logica/funcoes.c
+++ Logica/funcoes.c
@@ -1,16 +1,17 @@
-#include <dados.h>
+#include "dados.c"
+#include <stdlib.h>
 
-void inicia(ESTADO est){
+ESTADO* inicia(){
+	ESTADO *est = (ESTADO *) malloc(sizeof(ESTADO)); 
 	int i,j;
 	for(i = 0;i<8;i++)
 		for(j = 0;i<8;j++){
-			est.tab[i][j] = VAZIO;
+			est->tab[i][j] = VAZIO;
 
 		} 
-	est.pos.x = 4;
-	est.pos.y = 3;
-	est.tab[4][3] = BRANCA;
-	est.num_jogadas = 0;
-	est.jogador_atual = 1;
-
+	est->pos.x = 4;
+	est->pos.y = 3;
+	est->tab[4][3] = BRANCA;
+	est->num_jogadas = 0;
+	est->jogador_atual = 1;
 }
