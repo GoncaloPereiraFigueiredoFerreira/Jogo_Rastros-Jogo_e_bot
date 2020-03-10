41b90284911c77518c0c183f6604e3c6f9c304ca
GoncaloCoutoDosSantos <a93279@alunos.uminho.pt>
2020-03-10 17:09:09 +0000

    test

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h> 
+
+void desenhal(){ 
+	printf("  ");
+	for(int i = 0;i<8;i++) printf("+---");
+	putchar('+');
+	putchar('\n');
+}
+
+void desenha(char f[8][8]){
+	printf("    A   B   C   D   E   F   G   H\n");
+	for(int i = 0;i!=8;i++){
+		desenhal();
+		printf("%d ",i+1);
+		for(int j = 0;j!= 8;j++)
+			printf("| %c ",f[i][j]);
+		putchar('|');
+		putchar('\n');
+	}
+	desenhal();
+}
+
+int main(){
+	struct pos{int x,y;}pos;
+	pos.x = 4;
+	pos.y = 3;
+	char m[16];
+	char f[8][8] = {{'.','.','.','.','.','.','.','2'},
+	                {'.','.','.','.','.','.','.','.'},
+	                {'.','.','.','.','.','.','.','.'},
+	                {'.','.','.','.','*','.','.','.'},
+	                {'.','.','.','.','.','.','.','.'},
+	                {'.','.','.','.','.','.','.','.'},
+	                {'.','.','.','.','.','.','.','.'},
+	                {'1','.','.','.','.','.','.','.'}};
+
+	desenha(f);
+	for(int i = 0; m[i]!='\0';i += 2) printf("%c%c ",m[i],m[i+1]);
+
+	return 0;
+}
