6695c83cb46ca5fd4300a4c6d98389a64634c0ec
TiagoAndreLecaCarneiro <tiago.carneiro.email@gmail.com>
2020-03-25 15:51:07 +0000

    Função read

--- interface/interface.c
+++ interface/interface.c
@@ -52,6 +52,71 @@ int escolheVencedor(ESTADO *est){
 	return caso;
 }
 
+void read (char cam[],ESTADO *est){	
+	FILE *f;
+	char linha[10];
+	COORDENADA c;
+	int i=0,i2=0;
+	f = fopen (cam,"r");
+	while (i!=8){
+		fscanf(f,"%s",linha);
+		while (i2!=8){
+			if (linha[i2] == '#')
+			est->tab[i][i2] = linha[i2];
+			else if (linha[i2]== '*')
+			est->tab[i][i2] = linha[i2];
+			else est->tab[i][i2]=VAZIO;
+			i2++;
+		}
+		i++;
+		fscanf(f,"\n");
+	}
+	i=0;
+	fscanf(f,"\n");
+	fscanf(f,"%s",linha);
+	while (!feof(f)) {
+		fscanf(f,"\n");
+		fscanf(f,"%s",linha);
+		c = est->jogadas[i];
+		if (linha[5]!=EOF) {
+			c.x = linha[5];
+			c.y = linha[6];
+			i++;
+		}
+		if (linha[9]!=EOF){
+			c.x = linha[8];
+			c.y = linha[9];
+			i++;
+		}
+	}
+	desenha(est);
+	interpretador(est);
+}
+
+
+void save(char cam[],ESTADO *est){
+	FILE *f;
+	int x,y;
+	COORDENADA c;
+	f = fopen(cam,"w");
+	for (x = 0;x<8;x++){
+		for (y = 0;y<8;y++){
+			if (x == 7 && y == 0) fputc('2',f);
+			else if (x == 0 && y == 7) fputc('1',f); 
+			else fputc(est->tab[y][x],f);
+		}
+		fputc('\n',f);
+	}
+	y = est->num_jogadas;
+	for(x = 0;x<y;x++){
+		c = est->jogadas[x];
+		if (!(x%2)) fprintf(f,"\n%d%d: ",((x/2)+1)/10,((x/2)+1)%10);
+		fprintf(f,"%c%d ",c.x+'A',9-c.y);
+	}
+	fclose(f);
+}
+
+
 int interpretador(ESTADO *est) {
 	char linha[BUF_SIZE],cam[BUF_SIZE];
 	char col[2], lin[2];
@@ -71,6 +136,9 @@ int interpretador(ESTADO *est) {
 		COORDENADA c[8];
 		int n = moves(est,c);
 		for(int i = 0; i < n;i++) showCOORD(c[i]); 
+	}
+	else if (sscanf(linha,"ler %s",cam) == 1){
+		read(cam,est);
 	}
 	else if (sscanf(linha,"gr %s",cam) == 1){
 		save(cam,est);
--- interface/interface.h
+++ interface/interface.h
@@ -9,6 +9,7 @@ void showCOORD (COORDENADA c);
 void prompt (ESTADO *est);
 void desenhal();
 void desenha(ESTADO *est);
+void read(char cam[],ESTADO *est);
 int escolheVencedor(ESTADO *est);
 int interpretador(ESTADO *est);
 
