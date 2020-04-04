30199281c5ce6adf2c18cb053a34a13bbdba2a9b
GoncaloCoutoDosSantos0 <a93279@alunos.uminho.pt>
2020-04-04 21:54:48 +0100

    bot minmax alpha beta pruning

--- bot/bot.h
+++ bot/bot.h
@@ -2,11 +2,12 @@
 #define PL6_4_BOT_H
 
 #include "../Logica/funcoes.h"
-#define DEPTH 10000
+#define DEPTH 20
+#define MSCORE 2
 
 ESTADO* cpEst(ESTADO *est);
 int max(int v[],int n,int jog);
-int minmax(ESTADO *est,int jog,int depth);
+int minmax (ESTADO *est,int jog,int depth,int max,int min,int *t);
 COORDENADA bot (ESTADO *est);
 
 #endif
