ef9d5109507c379cb44eac4965abb18c026f19e3
GoncaloCoutoDosSantos <setefi@Setefi.localdomain>
2020-05-08 11:46:57 +0100

    bot ler

--- bot/interface.c
+++ bot/interface.c
@@ -34,13 +34,14 @@ int read(char cam[],ESTADO *est){
 	f = fopen(cam,"r");
 	int i,i1;
 	int c = 0;
+	char cam1[100];
 	if (f != NULL){
 		for (i = 0;i<8;i++){
-			if (fgets(cam,MAX,f)){
-			for(i1 = 0;cam[i1]!='\n' &&cam[i1]!='\0' ;i1++){
-				if (cam[i1] == '1' || cam[i1] == '2') est->tab[i][i1] = '.'; // atençao a isto em caso de mudar vazia
+			if (fgets(cam1,MAX,f)){
+			for(i1 = 0;cam1[i1]!='\n' && cam1[i1]!='\0' ;i1++){
+				if (cam1[i1] == '1' || cam1[i1] == '2') est->tab[i][i1] = '.'; // atençao a isto em caso de mudar vazia
 				else { est->tab[i1][i] = cam [i1];
-					if (cam[i1] == '*') {
+					if (cam1[i1] == '*') {
 						est->pos.x = i1;
 						est->pos.y = i;
 					}
@@ -50,11 +51,11 @@ int read(char cam[],ESTADO *est){
 		else return 1;
 	}
 
-		while (fgets(cam,BUF_SIZE,f) != NULL){
-			for(i1 = 0;cam[i1]!='\0';i1++){
-				if(cam[i1]>='a'&&cam[i1]<'i'){
-					est->jogadas[c].x = cam[i1]-'a';
-					est->jogadas[c++].y = 7-(cam[++i1]-'1');
+		while (fgets(cam1,BUF_SIZE,f) != NULL){
+			for(i1 = 0;cam1[i1]!='\0';i1++){
+				if(cam1[i1]>='a'&& cam1[i1]<'i'){
+					est->jogadas[c].x = cam1[i1]-'a';
+					est->jogadas[c++].y = 7-(cam1[++i1]-'1');
 				}
 			}
 		}
